5c33731581bd76621853e9b9e2ae1e10777fd8e2
NagiPragalathan <nagipragalathan@gmail.com>
2024-05-22 13:28:37 +0530

    scrolling bug fixed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -44,92 +44,126 @@ import threejs from '../assets/skill/threejs.png'
 import vue from '../assets/skill/vue.png'
 
 
-
 const Skills = () => {
     const settings = {
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 2000,
-        cssEase: 'linear',
-        infinite: true,
-        variableWidth: true,
-        arrows: false,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 0,
+      speed: 2000,
+      cssEase: 'linear',
+      infinite: true,
+      variableWidth: true,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
     };
-
+  
     const settings2 = {
-        slidesToShow: 5,
-        slidesToScroll: -1,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 2000,
-        cssEase: 'linear',
-        infinite: true,
-        variableWidth: true,
-        arrows: false,
-
-    }
-
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 0,
+      speed: 2000,
+      cssEase: 'linear',
+      infinite: true,
+      variableWidth: true,
+      arrows: false,
+      rtl: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    };
+  
     return (
-        <>
-            <section className="py-12 bg-black sm:py-16 lg:py-20 xl:py-24">
-                <Slider {...settings}>
-                    <div className='px-12'><img src={antd} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={aws} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={babel} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={bootstrap} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={cassandra} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={cloudflare} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={jquery} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={css} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={gcp} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={git} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={go} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={grafana} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={html} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={supabase} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={svelte} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={swagger} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={redux} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={solidity} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={tailwind} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={threejs} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={vue} alt="Tech Logo 1" /></div>
-                    {/* Add more items as needed */}
-                </Slider>
-
-            </section>
-            <section className="py-12 bg-black sm:py-16 lg:py-20 xl:py-24">
-                <Slider {...settings2}>
-                    <div className='px-12'><img src={dart} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={digitalocean} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={docker} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={django} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={elastic} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={firebase} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={flask} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={flutter} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={kubernetes} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={mongo} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={mysql} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={nest} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={next} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={node} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={openai} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={php} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={postgresql} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={python} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={qwik} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={react} alt="Tech Logo 1" /></div>
-                    <div className='px-12'><img src={redis} alt="Tech Logo 1" /></div>
-                    
-                    {/* Add more items a3 needed */}
-                </Slider>
-            </section>
-        </>
-
+      <>
+        <section className="py-12 bg-black sm:py-16 lg:py-20 xl:py-24">
+          <Slider {...settings}>
+            <div className='px-12'><img src={antd} alt="Tech Logo 1" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={aws} alt="Tech Logo 2" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={babel} alt="Tech Logo 3" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={bootstrap} alt="Tech Logo 4" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={cassandra} alt="Tech Logo 5" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={cloudflare} alt="Tech Logo 6" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={jquery} alt="Tech Logo 7" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={css} alt="Tech Logo 8" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={gcp} alt="Tech Logo 9" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={git} alt="Tech Logo 10" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={go} alt="Tech Logo 11" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={grafana} alt="Tech Logo 12" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={html} alt="Tech Logo 13" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={supabase} alt="Tech Logo 14" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={svelte} alt="Tech Logo 15" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={swagger} alt="Tech Logo 16" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={redux} alt="Tech Logo 17" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={solidity} alt="Tech Logo 18" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={tailwind} alt="Tech Logo 19" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={threejs} alt="Tech Logo 20" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={vue} alt="Tech Logo 21" className="w-full h-auto" /></div>
+          </Slider>
+        </section>
+        <section className="py-12 bg-black sm:py-16 lg:py-20 xl:py-24">
+          <Slider {...settings2}>
+            <div className='px-12'><img src={dart} alt="Tech Logo 22" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={digitalocean} alt="Tech Logo 23" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={docker} alt="Tech Logo 24" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={django} alt="Tech Logo 25" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={elastic} alt="Tech Logo 26" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={firebase} alt="Tech Logo 27" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={flask} alt="Tech Logo 28" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={flutter} alt="Tech Logo 29" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={kubernetes} alt="Tech Logo 30" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={mongo} alt="Tech Logo 31" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={mysql} alt="Tech Logo 32" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={nest} alt="Tech Logo 33" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={next} alt="Tech Logo 34" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={node} alt="Tech Logo 35" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={openai} alt="Tech Logo 36" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={php} alt="Tech Logo 37" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={postgresql} alt="Tech Logo 38" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={python} alt="Tech Logo 39" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={qwik} alt="Tech Logo 40" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={react} alt="Tech Logo 41" className="w-full h-auto" /></div>
+            <div className='px-12'><img src={redis} alt="Tech Logo 42" className="w-full h-auto" /></div>
+          </Slider>
+        </section>
+      </>
     );
-};
-
-export default Skills;
+  };
+  
+  export default Skills;
