b4f46e75bbf69bf5cbf251a7c0219f2d07592951
tomvj1258 <tomvj15@gmail.com>
2024-06-06 13:47:05 +0530

    Fix: Aboutus comp added button changed

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,57 @@
+const AboutUs = () => {
+   return (
+      <>
+         <section
+            className='py-12 bg-black sm:py-16 lg:py-20 xl:py-24'
+            id='about-us'
+         >
+            <div className='px-4 mx-auto sm:px-6 lg:px-16 max-w-7xl'>
+               <div className='max-w-5xl mx-auto  pb-16'>
+                  <h2 className='text-3xl font-normal text-white sm:text-4xl lg:text-5xl xl:text-6xl'>
+                     About Us
+                  </h2>
+                  <p className='mt-6 text-lg font-normal text-gray-400'>
+                     Welcome to DocChain DMS—the forefront of document
+                     management innovation. We are dedicated to transforming the
+                     way businesses manage, store, and share documents by
+                     harnessing the power of Artificial Intelligence (AI) and
+                     Blockchain technologies. Our mission is to lead the
+                     industry in providing secure and intelligent document
+                     management solutions.
+                  </p>
+               </div>
+
+               <div className='max-w-5xl mx-auto text-center pb-16'>
+                  <h2 className='text-3xl font-normal text-white sm:text-4xl lg:text-5xl xl:text-6xl'>
+                     Vision
+                  </h2>
+                  <p className='mt-6 text-lg font-normal text-gray-400'>
+                     To revolutionize the way businesses manage, store, and
+                     share documents by integrating cutting-edge AI and
+                     Blockchain technologies, aiming to become the leader in
+                     secure and intelligent document management solutions.
+                  </p>
+               </div>
+
+               <div className='max-w-5xl mx-auto text-center pb-16'>
+                  <h2 className='text-3xl font-normal text-white sm:text-4xl lg:text-5xl xl:text-6xl'>
+                     Mission
+                  </h2>
+                  <p className='mt-6 text-lg font-normal text-gray-400'>
+                     Our mission is to empower businesses with a robust, secure,
+                     and user-friendly Document Management System (DMS) that
+                     leverages the power of AI and Blockchain. We strive to
+                     streamline document management processes, enhance
+                     collaboration, ensure data security, and drive business
+                     efficiency. Our commitment lies in delivering innovative
+                     solutions that transform the way businesses operate, making
+                     document management a seamless experience.
+                  </p>
+               </div>
+            </div>
+         </section>
+      </>
+   );
+};
+
+export default AboutUs;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,68 +1,129 @@
 /* eslint-disable react/no-unknown-property */
-import { useState } from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import logo from '../assets/logo.svg';
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import logo from "../assets/logo.svg";
 
 function Nav() {
-    const [expanded, setExpanded] = useState(false);
-    return (
-        <section className="py-4 bg-black sm:py-6" x-data="{expanded: false}">
-            <div className="px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between">
-                    <div className="shrink-0">
-                        <a href="#" title="" className="flex">
-                            <img className="w-auto h-9" src={logo} alt="" />
-                        </a>
-                    </div>
+   const [expanded, setExpanded] = useState(false);
+   return (
+      <section className='py-4 bg-black sm:py-6' x-data='{expanded: false}'>
+         <div className='px-4 sm:px-6 lg:px-8'>
+            <div className='flex items-center justify-between'>
+               <div className='shrink-0'>
+                  <a href='#' title='' className='flex'>
+                     <img className='w-auto h-9' src={logo} alt='' />
+                  </a>
+               </div>
 
-                    <div className="flex md:hidden">
-                        <button type="button" className="text-white">
-                            {expanded ? <CloseIcon onClick={() => setExpanded(false)} /> : <MenuIcon onClick={() => setExpanded(true)} />}
-                        </button>
-                    </div>
+               <div className='flex md:hidden'>
+                  <button type='button' className='text-white'>
+                     {expanded ? (
+                        <CloseIcon onClick={() => setExpanded(false)} />
+                     ) : (
+                        <MenuIcon onClick={() => setExpanded(true)} />
+                     )}
+                  </button>
+               </div>
 
-                    <nav className="hidden space-x-10 md:flex md:items-center md:justify-center lg:space-x-12">
-                        <a href="/#solutions" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Our solutions </a>
+               <nav className='hidden space-x-10 md:flex md:items-center md:justify-center lg:space-x-12'>
+                  <a
+                     href='/#solutions'
+                     title=''
+                     className='text-base font-normal text-gray-400 transition-all duration-200 hover:text-white'
+                  >
+                     {" "}
+                     Our solutions{" "}
+                  </a>
 
-                        <a href="/#features" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Features </a>
+                  <a
+                     href='/#features'
+                     title=''
+                     className='text-base font-normal text-gray-400 transition-all duration-200 hover:text-white'
+                  >
+                     {" "}
+                     Features{" "}
+                  </a>
 
-                        <a href="/services" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Services </a>
+                  <a
+                     href='/services'
+                     title=''
+                     className='text-base font-normal text-gray-400 transition-all duration-200 hover:text-white'
+                  >
+                     {" "}
+                     Services{" "}
+                  </a>
 
-                        {/* <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Support </a> */}
-                    </nav>
+                  {/* <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Support </a> */}
+               </nav>
 
-                    <div className="relative hidden md:justify-center md:items-center md:inline-flex group">
-                        <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
-                        <a href="mailto:hello@docchain.cloud" title="" className="relative inline-flex items-center justify-center px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button"> Talk to Sales </a>
-                    </div>
-                </div>
-
-                {/* <!-- Mobile menu --> */
-                    expanded && (
-                        <nav>
-                            <div className="flex flex-col pt-8 pb-4 space-y-6">
-                                <a href="/#solutions" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Our solutions </a>
-
-                                <a href="/#features" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Features </a>
-
-                                <a href="/services" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Services </a>
-
-                                {/* <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white">  </a> */}
-
-                                <div className="relative inline-flex items-center justify-center group">
-                                    <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
-                                    <a href="mailto:hello@docchain.cloud" title="" className="relative inline-flex items-center justify-center w-full px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button"> Talk to Sales </a>
-                                </div>
-                            </div>
-                        </nav>
-                    )
-                }
+               <div className='relative hidden md:justify-center md:items-center md:inline-flex group'>
+                  <div className='absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50'></div>
+                  <a
+                     href='mailto:hello@docchain.cloud'
+                     title=''
+                     className='relative inline-flex items-center justify-center px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full'
+                     role='button'
+                  >
+                     {" "}
+                     Connect to Us{" "}
+                  </a>
+               </div>
             </div>
-        </section>
 
-    )
+            {
+               /* <!-- Mobile menu --> */
+               expanded && (
+                  <nav>
+                     <div className='flex flex-col pt-8 pb-4 space-y-6'>
+                        <a
+                           href='/#solutions'
+                           title=''
+                           className='text-base font-normal text-gray-400 transition-all duration-200 hover:text-white'
+                        >
+                           {" "}
+                           Our solutions{" "}
+                        </a>
+
+                        <a
+                           href='/#features'
+                           title=''
+                           className='text-base font-normal text-gray-400 transition-all duration-200 hover:text-white'
+                        >
+                           {" "}
+                           Features{" "}
+                        </a>
+
+                        <a
+                           href='/services'
+                           title=''
+                           className='text-base font-normal text-gray-400 transition-all duration-200 hover:text-white'
+                        >
+                           {" "}
+                           Services{" "}
+                        </a>
+
+                        {/* <a href="#" title="" className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white">  </a> */}
+
+                        <div className='relative inline-flex items-center justify-center group'>
+                           <div className='absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50'></div>
+                           <a
+                              href='mailto:hello@docchain.cloud'
+                              title=''
+                              className='relative inline-flex items-center justify-center w-full px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full'
+                              role='button'
+                           >
+                              {" "}
+                              Talk to Sales{" "}
+                           </a>
+                        </div>
+                     </div>
+                  </nav>
+               )
+            }
+         </div>
+      </section>
+   );
 }
 
-
-export default Nav
+export default Nav;
